3717aa1c961dbd6dca36bcc555074bc5f2f8be95
Luis Herrera <herrera.lrh@gmail.com>
2019-10-28 21:10:53 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,14 +1,1 @@
 require 'pry'
-
-def hello(array)
-  i = 0
-  collecton = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
-
-
-hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
